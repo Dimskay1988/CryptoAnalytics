b49b2cd91fe606f46aab8882859131acc6657991
anikeenko <dimskay1988@icloud.com>
2022-09-05 16:57:16 +0300

    edited bot.py

--- apps/Coin/management/commands/bot.py
+++ apps/Coin/management/commands/bot.py
@@ -12,7 +12,7 @@ def registration():
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    rmk = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
     rmk.add(types.KeyboardButton('Зарегистрироваться'), types.KeyboardButton('Использовать данные телеграм'))
 
     msg = bot.send_message(message.chat.id, 'Для того чтоб продолжить, выберите способ для регистрации',
@@ -24,13 +24,13 @@ def user_register(message):
     if message.text == 'Использовать данные телеграм':
         bot.send_message(message.chat.id,
                          f'Вы зарегистрированы как: {message.from_user.first_name} {message.from_user.last_name}')
-        Profile.objects.get_or_create(id_user=message.chat.id, name=message.from_user.first_name,
-                                      surname=message.from_user.last_name)
+        Profile.objects.get_or_create(id_user=message.chat.id, defaults={'name': message.from_user.first_name,
+                                                                         'surname': message.from_user.last_name})
     elif message.text == 'Зарегистрироваться':
         msg = bot.send_message(message.chat.id, 'Введите имя пользователя')
         bot.register_next_step_handler(msg, new_user_name)
     else:
-        bot.send_message(message.chat.id, 'Регистрация обязательна')
+        bot.send_message(message.chat.id, 'Пройдите пожалуйста регистрацию')
 
 
 def new_user_name(message):
@@ -41,15 +41,17 @@ def new_user_name(message):
 
 def new_last_name(message, last_name):
     first_name = message.text
-    # obj, created = Profile.objects.get_or_create(id_user=message.chat.id, name=last_name, surname=first_name, defaults={'id_user': message.chat.id, 'name': last_name, 'surname': first_name})
-    Profile.objects.get_or_create(id_user=message.chat.id, name=last_name, surname=first_name)
+    Profile.objects.get_or_create(id_user=message.chat.id, defaults={'name': last_name, 'surname': first_name})
     bot.send_message(message.chat.id, f'Отлично, вы зарегистрированы как {last_name} {first_name}')
 
 
-@bot.message_handler(commands=['user'])
-def user(message):
+@bot.message_handler(commands=['coin'])
+def coin(message):
     mess = f'{message.from_user.first_name} {message.from_user.last_name}'
     bot.send_message(message.chat.id, f'Привет {mess}')
+
+
+
 
 
 bot.polling(none_stop=True)
