eb9b57a4f33f0fc783846192b1e2cb80701d1572
anikeenko <dimskay1988@icloud.com>
2022-09-16 18:04:25 +0300

    edit bot.py add choice of currency and cryptocurrency

--- apps/Coin/management/commands/bot.py
+++ apps/Coin/management/commands/bot.py
@@ -21,15 +21,13 @@ def start(message):
     rmk.add(types.KeyboardButton('Зарегистрироваться'),
             types.KeyboardButton('Зарегистрироваться с ипользованием данных телеграм'),
             types.KeyboardButton('Просмотреть актуальный курс криптовалют'),
-            types.KeyboardButton('Отследить на повышение курса'),
-            types.KeyboardButton('Отследить на понижение курса'))
-
-    msg = bot.send_message(message.chat.id, 'Выберите что вы хотите сделать',
-                           reply_markup=rmk)
-    bot.register_next_step_handler(msg, user_message)
+            types.KeyboardButton('Выбрать криптовалюту и курс'),
+            types.KeyboardButton('HitGab'))
+    msg = bot.send_message(message.chat.id, 'Выберите что вы хотите сделать', reply_markup=rmk)
+    bot.register_next_step_handler(msg, user_reply)
 
 
-def user_message(message):
+def user_reply(message):
     if message.text == 'Зарегистрироваться с ипользованием данных телеграм':
         Profile.objects.get_or_create(id_user=message.chat.id, defaults={'name': message.from_user.first_name,
                                                                          'surname': message.from_user.last_name})
@@ -38,11 +36,19 @@ def user_message(message):
     elif message.text == 'Зарегистрироваться':
         msg = bot.send_message(message.chat.id, 'Введите имя пользователя')
         bot.register_next_step_handler(msg, new_user_name)
-    elif message.text == 'Отследить на повышение курса':
-        msg = bot.send_message(message.chat.id, 'Выберите какую валюту хотите отследить')
-        bot.register_next_step_handler(msg, coin_price_ap)
+    elif message.text == 'Выбрать криптовалюту и курс':
+        markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+        btn1 = types.KeyboardButton("Litecoin")
+        btn2 = types.KeyboardButton("Ethereum")
+        btn3 = types.KeyboardButton("Cardano")
+        btn4 = types.KeyboardButton("Tether")
+        btn5 = types.KeyboardButton("Bitcoin")
+        btn6 = types.KeyboardButton("Solana")
+        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+        msg = bot.send_message(message.chat.id, 'Выберите криптовалюту', reply_markup=markup)
+        bot.register_next_step_handler(msg, choice_cryptocurrency)
     elif message.text == 'Просмотреть актуальный курс криптовалют':
-        msg = bot.send_message(message.chat.id, 'Не понтяное')
+        msg = bot.send_message(message.chat.id, 'Данные')
         bot.register_next_step_handler(msg, coin_price)
     else:
         bot.send_message(message.chat.id, 'Пройдите пожалуйста регистрацию')
@@ -50,17 +56,17 @@ def user_message(message):
 
 def new_user_name(message):
     last_name = message.text
-    msg = bot.send_message(message.chat.id, f'Хорошо {last_name} введите вашу фамилию')
+    msg = bot.send_message(message.chat.id, f'Хорошо {last_name}, введите вашу фамилию')
     bot.register_next_step_handler(msg, new_last_name, last_name)
 
 
 def new_last_name(message, last_name):
     first_name = message.text
     Profile.objects.get_or_create(id_user=message.chat.id, defaults={'name': last_name, 'surname': first_name})
-    bot.send_message(message.chat.id, f'Отлично, вы зарегистрированы как {last_name} {first_name}')
+    msg = bot.send_message(message.chat.id, f'Отлично, вы зарегистрированы как {last_name} {first_name}')
+    bot.register_next_step_handler(msg, start)  # хз может и не надо
 
 
-@bot.message_handler(commands=['coin_price'])
 def coin_price(message):
     if Profile.objects.filter(id_user=message.chat.id).exists():
         data = Coins.objects.all()
@@ -69,30 +75,40 @@ def coin_price(message):
         for co in data:
             date = co.created_at
             coin += f'Криптавалюта: {co.name.upper()}, USD={co.usd}, EUR={co.eur}, UAH={co.uah}, CNY={co.cny}\n'
-        bot.send_message(message.chat.id, f'Актуальный курс {date}: \n{coin}')
+        msg = bot.send_message(message.chat.id, f'Актуальный курс {date}: \n{coin}')
+        bot.register_next_step_handler(msg, user_reply)
     else:
         msg = bot.send_message(message.chat.id, f'Пройдите пожалуйста регистрацию')
-        bot.register_next_step_handler(msg, user_message)
+        bot.register_next_step_handler(msg, user_reply)
 
 
-def coin_price_ap(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-    btn1 = types.KeyboardButton("Litecoin")
-    btn2 = types.KeyboardButton("Ethereum")
-    btn3 = types.KeyboardButton("Cardano")
-    btn4 = types.KeyboardButton("Tether")
-    btn5 = types.KeyboardButton("Bitcoin")
-    btn6 = types.KeyboardButton("Solana")
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+def choice_cryptocurrency(message):
     coin_up = message.text
-    print(coin_up)
-    msg = bot.send_message(message.chat.id, 'Выберите валюту в которой хотите отслеживать', reply_markup=markup)
-    currency_ap = message.text
-    print(currency_ap)
+    bot.send_message(message.chat.id, f'Вы выбрали {coin_up}')
+    if (coin_up.lower()) in ['bitcoin', 'litecoin', 'ethereum', 'solana', 'cardano', 'tether']:
+        rmk = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+        btn1 = types.KeyboardButton("USD")
+        btn2 = types.KeyboardButton("EUR")
+        btn3 = types.KeyboardButton("UAH")
+        btn4 = types.KeyboardButton("CNY")
+        rmk.add(btn1, btn2, btn3, btn4)
+        msg = bot.send_message(message.chat.id, f'Выберете валюту', reply_markup=rmk)
+        bot.register_next_step_handler(msg, choice_coin, coin_up)
+    else:
+        bot.send_message(message.chat.id, "Выберите из списка выше")
 
-    # data = Coins.objects.filter(name=coin_up).values(currency_ap).values_list()
-    # print(type(data))
-    # bot.send_message(message.chat.id, f'Курс {coin_up} = {data} {currency_ap}')
+
+def choice_coin(message, coin_up):
+    currency_ap = message.text
+    if (currency_ap.lower()) in ['usd', 'eur', 'uah', 'cny']:
+        bot.send_message(message.chat.id, f'Вы выбрали {coin_up} в {currency_ap}')
+        data = Coins.objects.filter(name=(coin_up.lower())).values((currency_ap.lower()))
+        well = ''
+        for i in data:
+            well += str(i[f'{currency_ap.lower()}'])
+        bot.send_message(message.chat.id, f'Актуальный курс {coin_up} {well} {currency_ap.upper()}')
+    else:
+        bot.send_message(message.chat.id, f'Вы не правильно выбрали валюту')
 
 
 bot.polling(none_stop=True)
