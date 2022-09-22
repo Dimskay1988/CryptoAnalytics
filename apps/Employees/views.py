c0e53cd4b0bc6dea209a508ab3c3feb219e1db90
anikeenko <dimskay1988@icloud.com>
2022-09-22 13:51:08 +0300

    edit app employees

--- apps/Employees/views.py
+++ apps/Employees/views.py
@@ -1,12 +1,15 @@
-#
-# from rest_framework.viewsets import ModelViewSet
-# from apps.Employees.models import CustomUser
-# from apps.Employees.serializers import UserSerializer
-#
-#
-# class UsersViewSet(ModelViewSet):
-#     """Список всех пользователей"""
-#     serializer_class = UserSerializer
-#     queryset = CustomUser.objects.all()
-#
-#
+from rest_framework.viewsets import ModelViewSet
+from apps.Employees.models import Profile, MessageProfile
+from apps.Employees.serializers import ProfileSerializer, MessageProfileSerializer
+
+
+class ProfileViewSet(ModelViewSet):
+    """list of all telegram bot users"""
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class MessageProfileViewSet(ModelViewSet):
+    """Список всех пользователей"""
+    serializer_class = MessageProfileSerializer
+    queryset = MessageProfile.objects.all()
