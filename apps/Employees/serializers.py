9279f6864bc27c46a522ceadd81ee374162c77ad
anikeenko <dimskay1988@icloud.com>
2022-09-26 19:50:22 +0300

    add registration function via telegram bot

--- apps/Employees/serializers.py
+++ apps/Employees/serializers.py
@@ -5,13 +5,26 @@ from .models import Profile, MessageProfile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ("id", "password", "username", "email")
+        fields = ('id', 'password', 'username', 'id_telegram', 'email')
         # fields = ['id', 'username', 'password', 'id_telegram', 'created_at']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ("id", "password", "username", "email")
+        fields = ('id', 'id_telegram', 'password', 'username', 'email')
+
+    def create(self, validated_data):
+        id_telegram = validated_data['id_telegram']
+        username = validated_data['username']
+        password = validated_data['password']
+        repeat_password = validated_data['repeat_password']
+        if password != repeat_password:
+            raise serializers.ValidationError(
+                {'password': 'Пароли не совпадают'})
+        user = Profile(username=username, id_telegram=id_telegram)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class MessageProfileSerializer(serializers.ModelSerializer):
@@ -30,19 +43,19 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            "username",
-            "password",
-            "repeat_password",
+            'username',
+            'password',
+            'repeat_password',
         ]
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        username = validated_data["username"]
-        password = validated_data["password"]
-        repeat_password = validated_data["repeat_password"]
+        username = validated_data['username']
+        password = validated_data['password']
+        repeat_password = validated_data['repeat_password']
         if password != repeat_password:
             raise serializers.ValidationError(
-                {"password": "Пароли не совпадают"})
+                {'password': 'Пароли не совпадают'})
         user = Profile(username=username)
         user.set_password(password)
         user.save()
